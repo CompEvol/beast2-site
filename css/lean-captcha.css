749b9a6617c2c7044bb6916761cd70de58cf2112
Remco Bouckaert <higgs.ml@gmail.com>
2018-02-28 12:23:25 +1300

    redo wordpress styling from scratch, leaving a lot of croft

---
+++ css/lean-captcha.css
@@ -0,0 +1,1 @@
+.lean-captcha-sr-only{position:absolute!important;width:1px!important;height:1px!important;margin:-1px!important;padding:0!important;overflow:hidden!important;clip:rect(0,0,0,0)!important;border:0!important}
